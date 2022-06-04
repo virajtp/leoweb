8f1c6d695e6f25417f597a377708e393c65f794e
virajtp <virajtharindu.pathirage@gmail.com>
2022-06-05 01:06:37 +0530

    a

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { Grid } from '@material-ui/core';
-// import SocialMediaButtons from 'react-social-media-buttons';
-// import { Container, Row, Col } from 'reactstrap';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faYoutube,
